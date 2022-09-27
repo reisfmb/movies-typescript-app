605d28e8f611ce1e7cb3f3305b6601868c31584e
Bruno Reis <reisfmb@gmail.com>
2022-09-27 20:10:36 +0200

    Added react icons package
    Created basic styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MoviesTable } from './components/MoviesTable';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
